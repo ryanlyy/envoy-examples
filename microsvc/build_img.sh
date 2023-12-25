e28ed942c7549b670bf55f9c72d45be7ea711383
root <ryanlyy@126.com>
2023-12-25 06:43:52 +0000

    update

---
+++ microsvc/build_img.sh
@@ -0,0 +1,11 @@
+#football-envoy-sidecar  football-flask  mainapp-envoy-sidecar  mainapp-flask
+WORK_DIR=$(pwd)
+cd $WORK_DIR/mainapp-flask
+podman build -f docker/Dockerfile -t livescores-microsvc:1.0 .
+cd $WORK_DIR/mainapp-envoy-sidecar
+podman build -f Dockerfile -t mainapp-envoy-sidecar:1.0 .
+cd $WORK_DIR/football-flask
+podman build -f Dockerfile -t footballscores:1.0 .
+cd $WORK_DIR/football-envoy-sidecar
+podman build -f Dockerfile -t football-envoy-sidecar:1.0 .
+
